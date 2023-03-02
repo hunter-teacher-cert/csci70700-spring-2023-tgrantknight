5b9d91d108bca9bae354895c7daa6d24e24ac6d1
Taylor Grant-Knight <97416649+tgrantknight@users.noreply.github.com>
2023-03-02 02:40:17 +0000

    hw

--- flask/main.py
+++ flask/main.py
@@ -1,13 +1,18 @@
 from flask import Flask
 from flask import render_template
+from flask import session
 import random
 
 app = Flask(__name__)
+app.secret_key = "somethingXD"
 
 
 @app.route('/')
 def index():
-  return render_template("home.html")
+  if "count" not in session:
+    session["count"] = 0
+  session["count"] += 1
+  return render_template("home.html",session=session)
 
 @app.route('/lucky')
 def lucky():
@@ -17,3 +22,4 @@ def lucky():
   return render_template("lucky.html", nums = numbers)
 
 app.run(host='0.0.0.0', port=81)
+
